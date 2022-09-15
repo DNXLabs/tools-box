f9900e4d5696731e2c298ad4a2e2b2fb7e825feb
marcusnogueira <marcus.nogueira@dnx.solutions>
2022-09-15 14:32:24 +1000

    Adding glacier restore script

--- scripts/clone-repositories-list/clone-repositories.sh
+++ scripts/clone-repositories-list/clone-repositories.sh
@@ -1,32 +1,9 @@
-#/bin/bash
+#!/bin/sh
 
-#Checking if necessary environment variables are set
-if [[ -z "${ORG_NAME}" ]] ; then
-  echo "ORG_NAME environment variable is not set, this variable is used to compose the repository URL"
-  exit
-fi
-
-if [[ -z "${VCS_URL}" ]] ; then
-  echo "VCS_URL environment variable is not set, this variable is used to compose the repository URL"
-  exit
-fi
-
-#List of private repositories and tags from arguments
-declare -a repos=("$@")
-
-# Disabe GIT advice
-git config --global advice.detachedHead false
-
-for PACKAGE in "${repos[@]}"; do
-    repoName="$(cut -d'@' -f1 <<< $PACKAGE)"
-    repoVersion="$(cut -d'@' -f2 <<< $PACKAGE)"
-    if [ -d "./local-repositories/$repoName" ] 
-        then
-            echo "Repository $repoName already exists locally." 
-        else
-            echo "Cloning git@$VCS_URL:$ORG_NAME/$repoName.git..."
-            git clone -b $repoVersion git@$VCS_URL:$ORG_NAME/$repoName.git ./local-repositories/$repoName
-    fi
-done   
-
- 
+IFS=$'\n'
+for x in `cat glacier-restore.txt`
+  do
+    echo "Begin restoring ${x}"
+    aws s3api restore-object --restore-request '{"Days":<numberofdays>,"GlacierJobParameters":{"Tier":"Standard"}}' --bucket <bucketname>  --key "$x"
+    echo "Done restoring ${x}"
+  done
